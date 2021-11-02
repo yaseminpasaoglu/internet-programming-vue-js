b77aaa758d12302c0324e948382d20edd4b68a92
Yasemin Paşaoğlu <79848832+yaseminpasaoglu@users.noreply.github.com>
2021-11-02 22:01:35 +0300

    first commit

---
+++ hello-world/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+ 
+    <Button></Button>
+ 
+</template>
+
+<script>
+import Button from './components/Button.vue';
+
+
+export default {
+  name: "App",
+  components: {
+    Button
+   
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ hello-world/src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+    <button v-on:click="hello">click here</button>
+</template>
+
+<script>    
+    export default{
+
+        name:'home',
+        props :{
+            data: String,
+            msg: String
+        },methods:{
+            hello()
+            {
+                alert("Hello World")
+            }
+        }
+    }
+
+</script>
+
+<style >
+button{
+    outline: none;
+    border-style: solid;
+    background: sandybrown;
+    position: relative;
+}
+</style>
